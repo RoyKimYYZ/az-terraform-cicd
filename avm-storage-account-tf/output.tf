f5d8fb86cb41d0b0f4c638e90deeb10c576e1d8a
Roy Kim <roy@roykim.ca>
2025-02-08 16:10:39 -0500

    Add Terraform configuration for AVM storage account, including .gitignore, README, and variable definitions for improved organization and usability

---
+++ avm-storage-account-tf/output.tf
@@ -0,0 +1,18 @@
+# Used to define output values that are useful for displaying information about your infrastructure after it has been created or updated. These output values can be used for several purposes:
+# Outputs can provide information about the resources created, such as resource names, IDs, or other attributes. This is useful for understanding the state of your infrastructure.
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+
+}
+
+output "location" {
+  value = azurerm_resource_group.rg.location
+
+}
+
+output "azurerm_client_config_current" {
+  value = data.azurerm_client_config.current
+}
+
+
